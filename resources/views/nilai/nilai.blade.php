bc46194e3ddec83aecd0eaae3452268bbfec7ca4
Josafat Pratama Susilo <susilojosafat@gmail.com>
2023-04-12 16:44:04 +0700

    Ubah panjang kolom jabatan pada migration, Edit view Nilai, Edit Seeder Nilai

--- resources/views/nilai/nilai.blade.php
+++ resources/views/nilai/nilai.blade.php
@@ -1,106 +1,107 @@
 @extends('layouts.template')
 @section('content')
-  <div class="content-wrapper">
+<div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Data Nilai</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item active">Data Nilai</li>
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Data Nilai</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item active">Data Nilai</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
     </section>
 
     <!-- Main content -->
     <section class="content">
 
-      <!-- Default box -->
-      <div class="card">
-        <div class="card-header">
-          <h3 class="card-title">Tabel Nilai</h3>
+        <!-- Default box -->
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Tabel Nilai</h3>
 
-          <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-              <i class="fas fa-minus"></i>
-            </button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-              <i class="fas fa-times"></i>
-            </button>
-          </div>
+                <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                        <i class="fas fa-minus"></i>
+                    </button>
+                    <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+                        <i class="fas fa-times"></i>
+                    </button>
+                </div>
+            </div>
+            <div class="card-body">
+
+                <a href="{{url('nilai/create')}}" class="btn btn-sm btn-success my-2">Tambah Data</a>
+
+                <table class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Nama</th>
+                            <th>NIM</th>
+                            <th>Proyek</th>
+                            <th>Manajemen Proyek</th>
+                            <th>Jaringan Komputer</th>
+                            <th>Kewarganegaraan</th>
+                            <th>PWL</th>
+                            <th>Praktikum Jarkom</th>
+                            <th>Statkom</th>
+                            <th>Business Intellegence</th>
+                            <th>ADBO</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @if($nilai->count() > 0)
+                        @foreach($nilai as $i => $m)
+                        <tr>
+                            <td>{{++$i}}</td>
+                            <td>{{$m->nama}}</td>
+                            <td>{{$m->nim}}</td>
+                            <td>{{$m->Proyek}}</td>
+                            <td>{{$m->Manajemen_Proyek}}</td>
+                            <td>{{$m->Jaringan_Komputer}}</td>
+                            <td>{{$m->Kewarganegaraan}}</td>
+                            <td>{{$m->PWL}}</td>
+                            <td>{{$m->Praktikum_Jarkom}}</td>
+                            <td>{{$m->Statkom}}</td>
+                            <td>{{$m->Business_Intellegence}}</td>
+                            <td>{{$m->ADBO}}</td>
+                            <td>
+                                <!-- Bikin tombol edit dan delete -->
+                                <a href="{{ url('/nilai/'. $m->id.'/edit') }}" class="btn btn-sm btn-warning">edit</a>
+
+                                <form method="POST" action="{{ url('/nilai/'.$m->id) }}">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-sm btn-danger">hapus</button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                        @else
+                        <tr>
+                            <td colspan="6" class="text-center">Data tidak ada</td>
+                        </tr>
+                        @endif
+                    </tbody>
+                </table>
+            </div>
+            <!-- /.card-body -->
+            <!-- /.card-body -->
+            <div class="card-footer">
+                ~Alfino Febry Krissaputra
+            </div>
+            <!-- /.card-footer-->
         </div>
-        <div class="card-body">
-
-          <a href="{{url('nilai/create')}}" class="btn btn-sm btn-success my-2">Tambah Data</a>
-
-          <table class="table table-bordered table-striped">
-            <thead>
-              <tr>
-                <th>No</th>
-                <th>Nama</th>
-                <th>NIM</th>
-                <th>Proyek</th>
-                <th>Manajemen Proyek</th>
-                <th>Jaringan Komputer</th>
-                <th>Kewarganegaraan</th>
-                <th>PWL</th>
-                <th>Praktikum Jarkom</th>
-                <th>Statkom</th>
-                <th>Business Intellegence</th>
-                <th>ADBO</th>
-              </tr>
-            </thead>
-            <tbody>
-              @if($nilai->count() > 0)
-                @foreach($nilai as $i => $m)
-                  <tr>
-                    <td>{{++$i}}</td>
-                    <td>{{$m->nama}}</td>
-                    <td>{{$m->nim}}</td>
-                    <td>{{$m->Proyek}}</td>
-                    <td>{{$m->Manajemen_Proyek}}</td>
-                    <td>{{$m->Jaringan_Komputer}}</td>
-                    <td>{{$m->Kewarganegaraan}}</td>
-                    <td>{{$m->PWL}}</td>
-                    <td>{{$m->Praktikum_Jarkom}}</td>
-                    <td>{{$m->Statkom}}</td>
-                    <td>{{$m->Business_Intellegence}}</td>
-                    <td>{{$m->ADBO}}</td>
-                    <td>
-                      <!-- Bikin tombol edit dan delete -->
-                      <a href="{{ url('/nilai/'. $m->id.'/edit') }}" class="btn btn-sm btn-warning">edit</a>
-
-                      <form method="POST" action="{{ url('/nilai/'.$m->id) }}" >
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-sm btn-danger">hapus</button>
-                      </form>
-                    </td>
-                  </tr>
-                @endforeach
-              @else
-                <tr><td colspan="6" class="text-center">Data tidak ada</td></tr>
-              @endif
-            </tbody>
-          </table>
-        </div>
-        <!-- /.card-body -->
-        <!-- /.card-body -->
-        <div class="card-footer">
-          ~Alfino Febry Krissaputra
-        </div>
-        <!-- /.card-footer-->
-      </div>
-      <!-- /.card -->
+        <!-- /.card -->
 
     </section>
     <!-- /.content -->
-  </div>
+</div>
 
 @endsection
-
