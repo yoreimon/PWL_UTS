a0aea0c39109a7ec96b85e7f9b7a799c4d04e651
AlfinoFebryKrissaputra <alfinofebry03@gmail.com>
2023-04-12 12:54:54 +0700

    edit route & view nilai

--- resources/views/nilai/nilai.blade.php
+++ resources/views/nilai/nilai.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.template')
+@extends('layouts.template')
 @section('content')
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [DashboardController::class, 'index']);
-Route::resource('nilai', NilaiController::class);
+Route::resource('/nilai', NilaiController::class);
